88983db301cf59187b0fa423f6a86496987d556a
samuel <samuelcontatodev@gmail.com>
2021-12-19 17:01:37 -0300

    nao salvar a mesma sala no banco

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -2,27 +2,42 @@ const Database = require('../db/config')
 
 module.exports = {
     async create(req, res) {
-        
+
         const db = await Database()
         const pass = req.body.password
 
         let roomId = ''
+        let isRoom = true
 
-        for(let i = 0; i < 6; i++) {
-            roomId += Math.floor(Math.random() * 10).toString()
+        while (isRoom) {
+            //gerando número da sala
+            for (let i = 0; i < 6; i++) {
+                roomId += Math.floor(Math.random() * 10).toString()
+            }
+
+            //verificando se sala já existe cadastrada no banco
+            //usamos all quando o banco retorna algo
+            const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+
+            //some verifica se existe essa condição
+            isRoom = roomsExistIds.some(roomExistId => roomExistId === roomId)
+
+            if (!isRoom) {
+
+                //inserindo a sala e a senha no banco de dados
+                await db.run(`INSERT INTO rooms (
+                    id,
+                    pass
+                ) VALUES (
+                    ${parseInt(roomId)},
+                    ${pass}
+                )`)
+            }
+    
         }
-
-        //inserindo no banco de dados
-        await db.run(`INSERT INTO rooms (
-            id,
-            pass
-        ) VALUES (
-            ${parseInt(roomId)},
-            ${pass}
-        )`)
 
         await db.close() //fechando conexão com o banco
 
-        res.redirect(`/room/${roomId}`)
+        res.redirect(`/room/${roomId}`)//redirecioando url
     }
 }
